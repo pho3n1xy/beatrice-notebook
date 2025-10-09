67d4decca5cc72ecce27149e8387a6c0a49706f2
pho3n1xy <echevarria.luis888@gmail.com>
2025-10-08 21:12:23 -0500

    We added the sign up form and changed the CSS on the button to match what we have. It is working now. I think I want to add an auth in with Google.

--- journal/views.py
+++ journal/views.py
@@ -176,4 +176,4 @@ class JournalEntryUpdateView(LoginRequiredMixin, SpillCounterMixin, UpdateView):
 class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'registration/sign.html'
+    template_name = 'registration/signup.html'
